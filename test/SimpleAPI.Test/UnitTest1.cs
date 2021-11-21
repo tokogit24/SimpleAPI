596be015da8f79b49e37f4c11803c46cae6d75a5
Rodrigue <rodytoko@gmail.com>
2021-11-21 12:53:15 -0600

    Initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController();
+        [Fact]
+        public void GetRetunsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Toko Rodrigue", returnValue.Value); 
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
